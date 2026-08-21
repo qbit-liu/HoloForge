779d75434ec7343ec5c137d76a2ec71f55b3a9d9
Xinyi <liuxinyi23@mails.ucas.ac.cn>
2026-08-21 20:09:05 +0800

    Report full HHH residual ladder on CI failure

--- tests/test_holographic_superconductor_optical.py
+++ tests/test_holographic_superconductor_optical.py
@@ -280,7 +280,7 @@ class OpticalNormalStateTests(unittest.TestCase):
             )
 
     def test_series_transferred_normal_state_passes_frozen_x_ladder(self) -> None:
-        primary = []
+        responses = []
         for degree in UV_TRANSFER_BULK_DEGREES:
             response = solve_series_transferred_spectral_response(
                 40.0, degree=degree, series_order=4
@@ -288,11 +288,52 @@ class OpticalNormalStateTests(unittest.TestCase):
             refinement = solve_series_transferred_spectral_response(
                 40.0, degree=degree, series_order=3
             )
-            primary.append(response)
-            self.assertLessEqual(
-                response.equation_residual,
-                UV_TRANSFER_EQUATION_TOLERANCE,
-            )
+            responses.append((response, refinement))
+
+        primary = [response for response, _ in responses]
+        diagnostic = json.dumps(
+            {
+                "ladder": [
+                    {
+                        "degree": response.degree,
+                        "equation_residual": response.equation_residual,
+                        "residual_coordinate": (
+                            response.bulk_element_residual.maximum_coordinate
+                        ),
+                        "transfer_field_residual": (
+                            response.transfer_field_residual
+                        ),
+                        "transfer_derivative_residual": (
+                            response.transfer_derivative_residual
+                        ),
+                        "horizon_boundary_residual": (
+                            response.horizon_boundary_residual
+                        ),
+                        "conditioning_roundoff_budget": (
+                            response.conditioning_roundoff_budget
+                        ),
+                        "conductivity_error": abs(response.conductivity - 1.0),
+                        "series_truncation_change": (
+                            abs(response.conductivity - refinement.conductivity)
+                            / (1.0 + abs(response.conductivity))
+                        ),
+                    }
+                    for response, refinement in responses
+                ],
+                "resolution_changes": [
+                    abs(right.conductivity - left.conductivity)
+                    / (1.0 + abs(left.conductivity))
+                    for left, right in zip(primary[:-1], primary[1:])
+                ],
+            },
+            sort_keys=True,
+        )
+        self.assertLessEqual(
+            max(response.equation_residual for response in primary),
+            UV_TRANSFER_EQUATION_TOLERANCE,
+            msg=diagnostic,
+        )
+        for response, refinement in responses:
             self.assertLessEqual(
                 response.transfer_field_residual,
                 UV_TRANSFER_ROW_TOLERANCE,
